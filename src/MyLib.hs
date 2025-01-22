12f0705a118eb4e6fb859ff9b82615f9aff87110
NzSN <nzsn0101@gmail.com>
2025-01-22 12:10:44 +0800

    U

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,11 +1,7 @@
-module MyLib (someFunc) where
+module MyLib where
 
 import Functoriality (BiFunctor(..), Contravariant(..))
 
 data Prod a b = Prod { left :: a, right :: b }
 instance BiFunctor Prod where
   bimap f g prod = Prod (f . left $ prod) (g . right $ prod)
-
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
